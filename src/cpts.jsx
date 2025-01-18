39aa340b74d324d9f01ec963daa7198d84258099
lanrengm <lanrengm@gmail.com>
2025-01-19 00:29:31 +0800

    -

--- src/cpts.jsx
+++ src/cpts.jsx
@@ -1,49 +1,50 @@
 /**
  * 一个分组
  */
-export class Foo extends Element {
+export class Top extends Element {
   screenW = Reactor.signal(1024);
   screenH = Reactor.signal(768);
 
   win = null;
+  winX = Reactor.signal(0);
   winY = Reactor.signal(0);
   winW = Reactor.signal(0);
   winH = Reactor.signal(0);
 
+  // 窗口到屏幕的边距
+  marginTop = Reactor.signal(0);
+  marginRight = Reactor.signal(0);
+  marginBottom = Reactor.signal(0);
+  marginLeft = Reactor.signal(0);
+
   title = Reactor.signal("");
   content = Reactor.signal("");
+  direction = Reactor.signal("horizontal");
 
-
-  this({ title = '福', content = '恭喜发财' }) {
+  this({ title = "福", content = "恭喜发财" }) {
     // 初始化屏幕相关数据
     const [screenW, screenH] = Window.this.screenBox("frame", "dimension");
     this.screenW.value = screenW;
     this.screenH.value = screenH;
     // 初始化窗口相关数据
     this.createWin();
-    Reactor.effect(() => {
-      const v= this.title.value;
-      if (this.win) {
-        this.win.caption = v;
-      }
-    });
-    Reactor.effect(() => {
-      const v = this.content.value;
-      if (this.win) {
-        this.win.document.$("#main").innerText = v;
-      }
-    });
     const [winX, winY, winW, winH] = this.win.box("xywh", "client", "monitor");
+    this.winX.value = winX;
     this.winY.value = winY;
     this.winW.value = winW;
     this.winH.value = winH;
-    this.win.on("closerequest", () => {
-      // Window.this.modal(<alert caption="警告">关闭{this.title}</alert>);
-      this.win = null;
-    });
+    this.win.on("closerequest", () => (this.win = null));
     // 初始化标题和内容
     this.title.value = title;
     this.content.value = content;
+    Reactor.effect(() => {
+      const v = this.title.value;
+      if (this.win) this.win.caption = v;
+    });
+    Reactor.effect(() => {
+      const v = this.content.value;
+      if (this.win) this.win.document.$("#main").innerText = v;
+    });
 
     // 响应窗口变化
     Reactor.effect(() => {
@@ -53,6 +54,26 @@ export class Foo extends Element {
       const newX = (this.screenW.value - stateW) / 2;
       if (this.win) {
         this.win.move(newX, stateY, stateW, stateH);
+      }
+    });
+    // 改宽高，变边距
+    Reactor.effect(() => {
+      const stateW = this.winW.value;
+      const stateH = this.winH.value;
+      // 宽变
+      
+    });
+    Reactor.effect(() => {
+      const v = this.direction.value;
+      if (this.win) {
+        this.win.document.$("#main").classList.remove("horizontal", "vertical");
+        this.win.document.$("#main").classList.add(v);
+      }
+    });
+    Reactor.effect(() => {
+      const winH = this.winH.value;
+      if (this.winY.value + winH > this.screenH.value) {
+        this.winY.value = this.screenH.value - winH;
       }
     });
   }
@@ -65,13 +86,259 @@ export class Foo extends Element {
       this.win = new Window({
         type: Window.POPUP_WINDOW,
         caption: this.title.value,
-        url: './dl.html',
+        url: "./dl.html",
         width: 400,
         height: 100,
         // parent: Window.this,
         alignment: 8,
       });
       this.win.document.$("#main").classList.add("horizontal");
+      this.win.document.$("#main").innerText = this.content.value;
+    }
+  }
+
+  showWin() {
+    if (this.win) {
+      this.win.state = Window.WINDOW_SHOWN;
+    } else {
+      this.createWin();
+    }
+  }
+
+  hiddenWin() {
+    if (this.win) {
+      this.win.state = Window.WINDOW_HIDDEN;
+    }
+  }
+
+  render() {
+    return (
+      <div class="group">
+        <details open>
+          <summary>{this.title}</summary>
+          <hr></hr>
+          <table>
+            <tr>
+              <td class="col-1">
+                <label for="title-text">分组标题</label>{" "}
+              </td>
+              <td class="col-end">
+                <input type="text" name="title-text" state-value={this.title} />
+              </td>
+            </tr>
+            <tr>
+              <td class="col-1">
+                <label for="top-text">内容</label>{" "}
+              </td>
+              <td class="col-end">
+                <input type="text" name="top-text" state-value={this.content} />
+              </td>
+            </tr>
+            <tr>
+              <td class="col-1">
+                <label for="direction">文字方向</label>
+              </td>
+              <td class="col-end">
+                <input
+                  type="radio"
+                  name="direction"
+                  id="horizontal"
+                  value="horizontal"
+                  checked={this.direction.value === "horizontal"}
+                  onchange={() => (this.direction.value = "horizontal")}
+                />
+                <label for="horizontal">水平方向</label>
+                <span>&nbsp;&nbsp;</span>
+                <input
+                  type="radio"
+                  name="direction"
+                  id="vertical"
+                  value="vertical"
+                  checked={this.direction.value === "vertical"}
+                  onchange={() => (this.direction.value = "vertical")}
+                />
+                <label for="vertical">垂直方向</label>
+              </td>
+            </tr>
+          </table>
+          <hr />
+          <table>
+            <tr>
+              <td class="col-1">
+                <label for="width">宽度</label>
+              </td>
+              <td class="col-2">
+                <input name="width" type="number" min="0" max={String(this.screenW)} step="1" state-value={this.winW} />
+              </td>
+              <td class="col-end">
+                <input type="hslider" min="0" max={String(this.screenW)} step="1" state-value={this.winW} />
+              </td>
+            </tr>
+            <tr>
+              <td class="col-1">
+                <label for="height">高度</label>
+              </td>
+              <td class="col-2">
+                <input name="height" type="number" min="0" max={String(this.screenH)} step="1" state-value={this.winH} />
+              </td>
+              <td class="col-end">
+                <input type="hslider" min="0" max={String(this.screenH)} step="1" state-value={this.winH} />
+              </td>
+            </tr>
+            <tr>
+              <td class="col-1">
+                <label for="top-margin">上边距</label>
+              </td>
+              <td class="col-2">
+                <input name="top-margin" type="number" min="0" max={String(this.screenH - this.winH)} value="0" step="1" state-value={this.marginTop} />
+              </td>
+              <td class="col-end">
+                <input type="hslider" min="0" max={String(this.screenH - this.winH)} value="0" step="1" name="top-margin" state-value={this.marginTop} />
+              </td>
+            </tr>
+            <tr>
+              <td class="col-1">
+                <label for="right-margin">右边距</label>
+              </td>
+              <td class="col-2">
+                <input name="right-margin" type="number" min="0" max={String(this.screenW - this.winW)} value="0" step="1" state-value={this.marginRight} />
+              </td>
+              <td class="col-end">
+                <input type="hslider" min="0" max={String(this.screenW - this.winW)} value="0" step="1" name="top-margin" state-value={this.marginRight} />
+              </td>
+            </tr>
+            <tr>
+              <td class="col-1">
+                <label for="top-margin">下边距</label>
+              </td>
+              <td class="col-2">
+                <input name="top-margin" type="number" min="0" max={String(this.screenH - this.winH)} value="0" step="1" state-value={this.marginBottom} />
+              </td>
+              <td class="col-end">
+                <input type="hslider" min="0" max={String(this.screenH - this.winH)} value="0" step="1" name="top-margin" state-value={this.marginBottom} />
+              </td>
+            </tr>
+            <tr>
+              <td class="col-1">
+                <label for="top-margin">左边距</label>
+              </td>
+              <td class="col-2">
+                <input name="top-margin" type="number" min="0" max={String(this.screenW - this.winW)} value="0" step="1" state-value={this.marginLeft} />
+              </td>
+              <td class="col-end">
+                <input type="hslider" min="0" max={String(this.screenW - this.winW)} value="0" step="1" name="top-margin" state-value={this.marginLeft} />
+              </td>
+            </tr>
+          </table>
+          <hr />
+          <table>
+            <tr>
+              <td class="col-1">
+                <label for="fgc">前景色</label>
+              </td>
+              <td class="col-end">
+                <input type="color" name="fgc" value="#000000" />
+              </td>
+            </tr>
+            <tr>
+              <td class="col-1">
+                <label for="bgc">背景色</label>
+              </td>
+              <td class="col-end">
+                <input type="color" name="bgc" value="#FFFFFF" />
+              </td>
+            </tr>
+          </table>
+        </details>
+        <table>
+          <tr>
+            <td class="col-1"></td>
+            <td class="col-2"></td>
+            <td class="col-end right">
+              <button onclick={this.showWin.bind(this)}>显示</button>
+              <button onclick={this.hiddenWin.bind(this)}>隐藏</button>
+            </td>
+          </tr>
+        </table>
+      </div>
+    );
+  }
+}
+
+export class Right extends Element {
+  screenW = Reactor.signal(1024);
+  screenH = Reactor.signal(768);
+
+  win = null;
+  winX = Reactor.signal(0);
+  winY = Reactor.signal(0);
+  winW = Reactor.signal(0);
+  winH = Reactor.signal(0);
+  winDX = Reactor.signal(0); // 右间距
+
+  title = Reactor.signal("");
+  content = Reactor.signal("");
+
+  this({ title = "福", content = "恭喜发财" }) {
+    try {
+      // 初始化屏幕相关数据
+      const [screenW, screenH] = Window.this.screenBox("frame", "dimension");
+      this.screenW.value = screenW;
+      this.screenH.value = screenH;
+      // 初始化窗口相关数据
+      this.createWin();
+      const [winX, winY, winW, winH] = this.win.box("xywh", "client", "monitor");
+      this.winX.value = winX;
+      this.winY.value = winY;
+      this.winW.value = winW;
+      this.winH.value = winH;
+      this.winDX.value = this.screenW.value - (winX + winW);
+      this.win.on("closerequest", () => (this.win = null));
+      // 初始化标题和内容
+      this.title.value = title;
+      this.content.value = content;
+      Reactor.effect(() => {
+        const v = this.title.value;
+        if (this.win) this.win.caption = v;
+      });
+      Reactor.effect(() => {
+        const v = this.content.value;
+        if (this.win) this.win.document.$("#main").innerText = v;
+      });
+
+      // 响应窗口变化
+      Reactor.effect(() => {
+        const stateX = this.winX.value;
+        const stateY = this.winY.value;
+        const stateW = this.winW.value;
+        const stateH = this.winH.value;
+
+        const stateDX = this.winDX.value;
+        const newX = this.screenW.value - stateDX - stateW;
+        if (this.win) {
+          this.win.move(newX, stateY, stateW, stateH);
+        }
+      });
+    } catch (e) {
+      Window.this.modal(<info>{e}</info>);
+    }
+  }
+
+  /**
+   * 初始化窗口，或者当窗口被意外关闭后重新创建窗口。
+   */
+  createWin() {
+    if (!this.win) {
+      this.win = new Window({
+        type: Window.POPUP_WINDOW,
+        caption: this.title.value,
+        url: "./dl.html",
+        width: 100,
+        height: 600,
+        // parent: Window.this,
+        alignment: 6,
+      });
+      // this.win.document.$("#main").classList.add("horizontal");
       this.win.document.$("#main").innerText = this.content.value;
     }
   }
@@ -115,13 +382,13 @@ export class Foo extends Element {
             </tr>
             <tr>
               <td class="col-1">
-                <label for="top-margin">上边距</label>
+                <label for="top-margin">右边距</label>
               </td>
               <td class="col-2">
-                <input type="number" min="0" max={String(this.screenH)} value="0" step="1" state-value={this.winY} />
+                <input type="number" min="0" max={String(this.screenW - this.winW)} value="0" step="1" state-value={this.winDX} />
               </td>
-              <td class="col-3">
-                <input type="hslider" min="0" max={String(this.screenH)} value="0" step="1" name="top-margin" state-value={this.winY} />
+              <td class="col-end">
+                <input type="hslider" min="0" max={String(this.screenW - this.winW)} value="0" step="1" name="top-margin" state-value={this.winDX} />
               </td>
             </tr>
             <tr>
@@ -131,7 +398,7 @@ export class Foo extends Element {
               <td class="col-2">
                 <input type="number" min="0" max={String(this.screenW)} step="1" state-value={this.winW} />
               </td>
-              <td class="col-3">
+              <td class="col-end">
                 <input type="hslider" min="0" max={String(this.screenW)} step="1" state-value={this.winW} />
               </td>
             </tr>
@@ -142,7 +409,7 @@ export class Foo extends Element {
               <td class="col-2">
                 <input type="number" min="0" max={String(this.screenH)} step="1" state-value={this.winH} />
               </td>
-              <td class="col-3">
+              <td class="col-end">
                 <input type="hslider" min="0" max={String(this.screenH)} step="1" state-value={this.winH} />
               </td>
             </tr>
@@ -168,7 +435,7 @@ export class Foo extends Element {
           <tr>
             <td class="col-1"></td>
             <td class="col-2"></td>
-            <td class="col-3 right">
+            <td class="col-end right">
               <button onclick={this.showWin.bind(this)}>显示</button>
               <button onclick={this.hiddenWin.bind(this)}>隐藏</button>
             </td>
--- src/index.jsx
+++ src/index.jsx
@@ -1,352 +1,39 @@
 import * as sys from "@sys";
 import { encode, decode } from "@sciter";
 
-import { Foo } from "./cpts.jsx";
+import { Top, Right } from "./cpts.jsx";
 
-const dlPath = 'dl.html';
 const dataPath = 'data.json';
 
-class ChineseCouplet {
-  topText = "";
-  rightText = "";
-  leftText = "";
 
-  topWin = null;
-  rightWin = null;
-  leftWin = null;
-
-  initTopWin() {
-    if (!this.topWin) {
-      this.topWin = new Window({
-        type: Window.POPUP_WINDOW,
-        caption: "横批",
-        url: dlPath,
-        width: 400,
-        height: 100,
-        // parent: Window.this,
-        alignment: 8,
-      });
-      this.topWin.document.$("#main").classList.add("horizontal");
-      this.topWin.document.$("#main").innerText = this.topText;
-      this.topWin.on('closerequest', () => {
-        Window.this.modal(<alert caption="警告">关闭横批</alert>);
-        this.topWin = null;
-      });
-    }
-  }
-
-  initRightWin() {
-    if (!this.rightWin) {
-      this.rightWin = new Window({
-        type: Window.POPUP_WINDOW,
-        caption: "上联",
-        url: dlPath,
-        width: 100,
-        height: 400,
-        // parent: Window.this,
-        alignment: 6,
-      });
-      this.rightWin.document.$("#main").classList.add("vertical");
-      this.rightWin.document.$("#main").innerText = this.rightText;
-      this.rightWin.on('closerequest', () => {
-        Window.this.modal(<alert caption="警告">关闭上联</alert>);
-        this.rightWin = null;
-      });
-    }
-  }
-
-  initLeftWin() {
-    if (!this.leftWin) {
-      this.leftWin = new Window({
-        type: Window.POPUP_WINDOW,
-        caption: "下联",
-        url: dlPath,
-        width: 100,
-        height: 400,
-        // parent: Window.this,
-        alignment: 4,
-      });
-      this.leftWin.document.$("#main").classList.add("vertical");
-      this.leftWin.document.$("#main").innerText = this.leftText;
-      this.leftWin.on('closerequest', () => {
-        Window.this.modal(<alert caption="警告">关闭下联</alert>);
-        this.leftWin = null;
-      });
-    }
-  }
-
-  init(data = { topText: "", rightText: "", leftText: "" }) {
-    // this.topText = data.topText;
-    this.rightText = data.rightText;
-    this.leftText = data.leftText;
-    // this.initTopWin();
-    this.initRightWin();
-    this.initLeftWin();
-  }
-
-  show() {
-    // if (this.topWin) {
-    //   this.topWin.state = Window.WINDOW_SHOWN;
-    //   this.topWin.document.$("#main").innerText = this.topText;
-    // } else {
-    //   this.initTopWin();
-    // }
-    if (this.rightWin) {
-      this.rightWin.state = Window.WINDOW_SHOWN;
-      this.rightWin.document.$("#main").innerText = this.rightText;
-    } else {
-      this.initRightWin();
-    }
-    if (this.leftWin) {
-      this.leftWin.state = Window.WINDOW_SHOWN;
-      this.leftWin.document.$("#main").innerText = this.leftText;
-    } else {
-      this.initLeftWin();
-    }
-  }
-
-  hidden() {
-    if (this.topWin) {
-      this.topWin.state = Window.WINDOW_HIDDEN;
-    }
-    if (this.rightWin) {
-      this.rightWin.state = Window.WINDOW_HIDDEN;
-    }
-    if (this.leftWin) {
-      this.leftWin.state = Window.WINDOW_HIDDEN;
-    }
-  }
-}
-
-document.on('change', 'input#top-text', function (evt) {
-  // Window.this.modal(<info>{evt.target.value}</info>);;
-  if (chineseCouplet.topWin) {
-    const el = chineseCouplet.topWin.document.$('#main');
-    el.innerText = evt.target.value;
-  }
-});
-document.on('change', 'input#right-text', function (evt) {
-  // Window.this.modal(<info>{evt.target.value}</info>);;
-  if (chineseCouplet.rightWin) {
-    const el = chineseCouplet.rightWin.document.$('#main');
-    el.innerText = evt.target.value;
-  }
-});
-document.on('change', 'input#left-text', function (evt) {
-  // Window.this.modal(<info>{evt.target.value}</info>);;
-  if (chineseCouplet.leftWin) {
-    const el = chineseCouplet.leftWin.document.$('#main');
-    el.innerText = evt.target.value;
-  }
-});
 document.on('click', 'button#save', async function () {
-  const config = {
-    topText: document.$('input#top-text').value,
-    rightText: document.$('input#right-text').value,
-    leftText: document.$('input#left-text').value,
-  };
-  const json = JSON.stringify(config, null, 2);
-  const file = await sys.fs.open(dataPath, "w+", 0o666);
-  await file.write(json);
-  await file.close();
-  document.$('#result').innerText = `保存数据成功!`;
+  // const config = {
+  //   topText: document.$('input#top-text').value,
+  //   rightText: document.$('input#right-text').value,
+  //   leftText: document.$('input#left-text').value,
+  // };
+  // const json = JSON.stringify(config, null, 2);
+  // const file = await sys.fs.open(dataPath, "w+", 0o666);
+  // await file.write(json);
+  // await file.close();
+  Window.this.modal(<alert caption="保存成功">配置文件已保存到 {dataPath}</alert>);
 });
 
-
-
-function MainGroup({ chineseCouplet: chineseCouplet }) {
-  return (
-    <details open class="group">
-      <summary>整体设置</summary>
-      <hr></hr>
-      <table >
-        <tr>
-          <td class="col-1"></td>
-          <td class="col-2"></td>
-        </tr>
-        <tr>
-          <td class="col-1">上联</td>
-          <td class="col-2">
-            <button onclick={() => chineseCouplet.rightWin.state = Window.WINDOW_SHOWN}>显示</button>
-            <button onclick={() => chineseCouplet.rightWin.state = Window.WINDOW_HIDDEN}>隐藏</button>
-          </td>
-        </tr>
-        <tr>
-          <td class="col-1">下联</td>
-          <td class="col-2">
-            <button onclick={() => chineseCouplet.leftWin.state = Window.WINDOW_SHOWN}>显示</button>
-            <button onclick={() => chineseCouplet.leftWin.state = Window.WINDOW_HIDDEN}>隐藏</button>
-          </td>
-        </tr>
-        <tr>
-          <td class="col-1"></td>
-          <td class="col-2">
-            <button onclick={chineseCouplet.show.bind(chineseCouplet)}>显示全部</button>
-            <button onclick={chineseCouplet.hidden.bind(chineseCouplet)}>隐藏全部</button>
-          </td>
-        </tr >
-      </table>
-    </details>
-  );
-}
-
-function TopGroup({ chineseCouplet: chineseCouplet }) {
-  const state = {
-    numberEl: null,
-    sliderEl: null,
-    _value: 0,
-    get value() {
-      return this._value;
-    },
-    set value(newValue) {
-      try {
-        document.$('#result').innerText = `new Value: ${newValue}`;
-        if (newValue !== this._value) {
-          this._value = newValue;
-          const [x, y] = chineseCouplet.topWin.box("position", "client", "monitor")
-          chineseCouplet.topWin.move(x, newValue);
-        }
-        if (newValue !== this.numberEl.value) {
-          this.numberEl.value = newValue;
-        }
-        if (newValue !== this.sliderEl.value) {
-          this.sliderEl.value = newValue;
-        }
-      } catch (e) {
-        Window.this.modal(<alert caption="报错">{e}</alert>);
-      }
-
-    }
-  }
-  let title = "横批设置";
-  return (
-    <div class="group">
-      <details open>
-        <summary>{title}</summary>
-        <hr></hr>
-        <table>
-          <tr>
-            <td class="col-1"><label for="top-text">内容</label></td>
-            <td class="col-2">
-              <input type="text" name="top-text" id="top-text" />
-            </td>
-          </tr>
-          <tr>
-            <td class="col-1"><label for="top-margin">上边距</label></td>
-            <td class="col-2">
-              <input type="hslider" min="0" max="1000" value="0" step="1"
-                var={state.sliderEl} name="top-margin"
-                onchange={(evt) => state.value = evt.target.value}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td class="col-1" ></td>
-            <td class="col-2">
-              <input type="number" min="0" max="1000" value="0" step="1"
-                var={state.numberEl}
-                onchange={(evt) => state.value = evt.target.value}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td class="col-1"><label for="">宽度</label></td>
-            <td class="col-2">
-              <input type="hslider" min="0" max="1000" value="0" step="1" />
-            </td>
-          </tr>
-          <tr>
-            <td class="col-1"></td>
-            <td class="col-2">
-              <input type="number" min="0" max="1000" value="0" step="1"
-              // var={}
-              // onchange={}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td class="col-1"><label for="">高度</label></td>
-            <td class="col-2">
-              <input type="hslider" min="0" max="1000" value="0" step="1" />
-            </td>
-          </tr>
-          <tr>
-            <td class="col-1"></td>
-            <td class="col-2">
-              <input type="number" min="0" max="1000" value="0" step="1"
-              // var={}
-              // onchange={}
-              />
-            </td>
-          </tr>
-          <tr>
-            <td class="col-1"><label for="fgc">前景色</label></td>
-            <td class="col-2">
-              <input type="color" value="black" name="fgc" id="" />
-            </td>
-          </tr>
-          <tr>
-            <td class="col-1"><label for="bgc">背景色</label></td>
-            <td class="col-2"><input type="color" value="red" name="bgc" /></td>
-          </tr>
-        </table>
-      </details>
-      <table>
-        <tr>
-          <td class="col-1"></td>
-          <td class="col-2">
-            <button onclick={() => chineseCouplet.topWin.state = Window.WINDOW_SHOWN}>显示</button>
-            <button onclick={() => chineseCouplet.topWin.state = Window.WINDOW_HIDDEN}>隐藏</button>
-          </td>
-        </tr>
-      </table>
-    </div>
-  );
-}
-
-function RightGroup({ chineseCouplet: chineseCouplet }) {
-  return (
-    <details open class="group">
-      <summary>上联设置</summary>
-      <hr></hr>
-      <table>
-        <tr>
-          <td class="col-1"><label for="right-text">上联</label></td>
-          <td class="col-2">
-            <input type="text" name="right-text" id="right-text" />
-          </td>
-        </tr>
-      </table>
-    </details>
-  );
-}
 
 document.on('ready', async function () {
-  const chineseCouplet = new ChineseCouplet();
   try {
-    document.$('#root').append(<Foo title="横批" content="恭喜发财" />);
+    document.$('#root').append(<Top title="横批" content="恭喜发财" />);
+    document.$('#root').append(<Right title="上联" content="福临宝地千秋盛" />);
   } catch (e) {
     Window.this.modal(<alert caption="报错">{e}</alert>);
   }
-  document.$('#root').append(<MainGroup chineseCouplet={chineseCouplet} />);
-  document.$('#root').append(<TopGroup chineseCouplet={chineseCouplet} />);
-  document.$('#root').append(<RightGroup chineseCouplet={chineseCouplet} />);
+
+  // 加载文件
   try {
     const file = await sys.fs.open(dataPath, "r", 0o666);
     const data = JSON.parse(decode(await file.read(), "utf8"));
     await file.close();
-    document.$('input#top-text').value = data.topText;
-    document.$('input#right-text').value = data.rightText;
-    document.$('input#left-text').value = data.leftText;
-
-    document.$('#result').appendChild(document.createTextNode('数据加载成功!'));
   } catch (e) {
     Window.this.modal(<alert caption="警告">第一次启动，没有配置文件</alert>);
   }
-  chineseCouplet.init({
-    topText: document.$('input#top-text').value,
-    rightText: document.$('input#right-text').value,
-    leftText: document.$('input#left-text').value,
-  });
-  chineseCouplet.show();
 });

cf00784ff041052aea80c60e129de6191258d36c
lanrengm <lanrengm@gmail.com>
2025-01-20 17:58:22 +0800

    1. 修改 ReadMe.md 文档，显示实际使用效果 2. 设计并采用原创 Logo 3. 添加系统托盘图标

--- README.md
+++ README.md
@@ -1,12 +1,5 @@
+# 春联，对联，电子春联，赛博春联，电子对联，赛博对联
 
+![示例](./doc/assets/example.png)
 
-## 图标
-
-![](assets/snake_512.ico)
-
-- 来源： https://icon-icons.com/zh/%E5%9B%BE%E6%A0%87/%E8%9B%87/100855
-- 许可： CC Atribution
-
-![](assets/yh_64.ico)
-- 来源： https://icon-icons.com/zh/%E5%9B%BE%E6%A0%87/%E9%A5%BC%E5%B9%B2-%E6%8E%92%E7%81%AF%E8%8A%82-%E7%83%9F%E8%8A%B1-%E5%BA%86%E7%A5%9D%E6%B4%BB%E5%8A%A8-%E8%8A%82%E6%97%A5/197210
-- 许可： CC Atribution
+![控制台](./doc/assets/console.png)
--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,23 @@ import { Foo } from "./cpts.jsx";
 
 document.on("ready", async function () {
   try {
+    // 设置系统托盘图标
+    const data = await sys.fs.readFile("./assets/foo_512.png");
+    Window.this.trayIcon({
+      image: Graphics.Image.fromBytes(data),
+      text: "赛博对联\nhi\nhi",
+    });
+    Window.this.on("trayiconclick", (evt) => {
+      // Window.this.modal(<info>trayiconclick {evt}</info>);
+      if (Window.this.state === Window.WINDOW_MINIMIZED) {
+        Window.this.state = Window.WINDOW_SHOWN;
+      } else if(Window.this.state === Window.WINDOW_SHOWN) {
+        Window.this.state = Window.WINDOW_HIDDEN;
+      } else {
+        Window.this.state = Window.WINDOW_SHOWN;
+      }
+    });
+
     // 设置控制台窗口初始位置
     const [screenW, screenH] = Window.this.screenBox("frame", "dimension");
     Window.this.move(screenW * 0.65, screenH * 0.05);
